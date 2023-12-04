c403efd7d67d1021425b8482a3ebaddb01ac614b
Ariel Zusman <ariel.zusman@gmail.com>
2023-12-04 10:08:30 +0200

    fix minuteSconds pipe

--- src/app/minute-seconds.pipe.ts
+++ src/app/minute-seconds.pipe.ts
@@ -1,10 +1,13 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({
   name: 'minuteSeconds',
 })
 export class MinuteSecondsPipe implements PipeTransform {
   transform(value: number): string {
-  return value.toString();
+    const minutes = Math.floor(value / 60);
+    const seconds = value % 60;
+
+    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
   }
 }
