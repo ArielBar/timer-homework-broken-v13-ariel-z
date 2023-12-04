35e4cab1cf52f58dacaf0c94ddbdf7d6f1f8fa86
Ariel Zusman <ariel.zusman@gmail.com>
2023-12-04 15:42:36 +0200

    fix runTimers function to only emit a value when there is a running timer. Also remove unnecessary iterations on the timers array

--- src/app/timer-manager.service.ts
+++ src/app/timer-manager.service.ts
@@ -1,53 +1,68 @@
-import { Injectable, NgZone } from '@angular/core';
-import { Observable, timer, BehaviorSubject } from 'rxjs';
-import { tap, filter } from 'rxjs/operators';
+import {Injectable} from '@angular/core';
+import {BehaviorSubject, EMPTY, Observable, of, Subscription, switchMap, timer} from 'rxjs';
+import {tap} from 'rxjs/operators';
+
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class TimerManagerService {
-  readonly initialValue = 0;
-  private timers: {
-    id: number;
-    subj$: BehaviorSubject<number>;
-    isRunning: boolean;
-  }[] = [];
-  constructor(private ngZone: NgZone) {
-    this.runTimers(); //FIXME: can be a performance issue
-  }
+    readonly initialValue = 0;
+    private timers: {
+        id: number;
+        subj$: BehaviorSubject<number>;
+        isRunning: boolean;
+    }[] = [];
 
-  getTimer(id: number): Observable<number> {
-    const newTimer = this.createTimer();
-    this.timers.push({ id, subj$: newTimer, isRunning: false });
-    return newTimer.asObservable();
-  }
-  private createTimer() {
-    return new BehaviorSubject<number>(this.initialValue);
-  }
-  public playTimer(id: number): void {
-    const timer = this.timers.find((x) => x.id === id);
-    if (timer) {
-      timer.isRunning = true;
-    }
-  }
-  public pauseTimer(id: number): void {
-    const timer = this.timers.find((x) => x.id === id);
-    if (timer) {
-      timer.isRunning = false;
-    }
-  }
+    private timerSubscription: Subscription | undefined;
 
-  private runTimers(): void {
-    timer(0, 1000)
-      .pipe(
-        filter((x) => this.timers.findIndex((y) => y.isRunning) >= 0),
-        tap(() => {
-          this.ngZone.run(() => {
-            this.timers
-              .filter((x) => x.isRunning)
-              .forEach((subj) => subj.subj$.next(subj.subj$.value + 1));
-          });
-        })
-      )
-      .subscribe();
-  }
+    getTimer(id: number): Observable<number> {
+        const newTimer = this.createTimer();
+        this.timers.push({id, subj$: newTimer, isRunning: false});
+        return newTimer.asObservable();
+    }
+
+    private createTimer() {
+        return new BehaviorSubject<number>(this.initialValue);
+    }
+
+    public playTimer(id: number): void {
+        const timer = this.timers.find((x) => x.id === id);
+        if (timer) {
+            timer.isRunning = true;
+            this.runTimers();
+        }
+    }
+
+    public pauseTimer(id: number): void {
+        const timer = this.timers.find((x) => x.id === id);
+        if (timer) {
+            timer.isRunning = false;
+            this.stopTimers()
+        }
+    }
+
+    private runTimers(): void {
+        if (this.timerSubscription) {
+            return;
+        }
+
+        this.timerSubscription = timer(0, 1000)
+            .pipe(
+                switchMap(() => {
+                    const runningTimer = this.timers.find((timer) => timer.isRunning);
+                    return runningTimer ? of(runningTimer) : EMPTY;
+                }),
+                tap((runningTimer) => {
+                    runningTimer.subj$.next(runningTimer.subj$.value + 1);
+                })
+            )
+            .subscribe();
+    }
+
+    private stopTimers(): void {
+        if (this.timerSubscription) {
+            this.timerSubscription.unsubscribe();
+            this.timerSubscription = undefined;
+        }
+    }
 }
